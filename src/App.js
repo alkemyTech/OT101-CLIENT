143e31015954b7b8345291df5bf996b3fbfa8d0e
alejandropascuale <apascuale@outlook.com>
2021-11-24 11:07:47 -0300

    add Routes to manage wrong ulrs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import './App.css';
+import Error404 from './components/Error404';
 import Home from './components/Home';
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path='/' exact={true} component={Home} />
-      </Switch>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        {/* returns the Erorr404 component in case the path does't exist */}
+        <Route path='*' element={<Error404/>} />
+      </Routes>
     </BrowserRouter>
   );
 }
