9b765de40191057217b263eb74274d2e907df95b
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2021-11-24 20:20:54 -0300

    Add formik field validators

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -12,6 +12,24 @@ export function Contact() {
     validate: (values) => {
       const errors = {};
 
+      if (!values.name) {
+        errors.name = 'Debes ingresar un nombre';
+      }
+
+      if (!values.lastname) {
+        errors.lastname = 'Debes ingresar un apellido';
+      }
+
+      if (!values.email) {
+        errors.email = 'Debes ingresar un email';
+      } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Ingresa un email válido';
+      }
+
+      if (!values.content) {
+        errors.content = 'Debes ingresar la consulta';
+      }
+
       return errors;
     },
     onSubmit: (values) => {
