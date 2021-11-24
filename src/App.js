94e59baefd2ed34999fc7a982adea2df606f81d4
alejandropascuale <apascuale@outlook.com>
2021-11-24 12:02:12 -0300

    create components to render routes and set routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import './App.css';
+import About from './components/About';
+import Activities from './components/Activities';
+import Contact from './components/Contact';
+import Contribute from './components/Contribute';
 import Error404 from './components/Error404';
 import Home from './components/Home';
+import News from './components/News';
+import Testimonials from './components/Testimonials';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/activities' element={<Activities/>} />
+        <Route path='/news' element={<News/>} />
+        <Route path='/testimonials' element={<Testimonials/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/contribute' element={<Contribute/>} />
         {/* returns the Erorr404 component in case the path does't exist */}
         <Route path='*' element={<Error404/>} />
       </Routes>
---
+++ src/components/Activities.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Activities() {
+    return (
+        <>
+            <h1>Activities...</h1>    
+        </>
+    )
+}
+
+export default Activities
