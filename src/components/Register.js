30af030082755ee2aacac0b5a1e4c1fc7894d8a1
germansanz93 <germansanz93@gmail.com>
2021-11-26 11:35:05 -0300

    formik validations done

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, TextField } from '@material-ui/core'
+import { Box, TextField, Button } from '@material-ui/core'
 import { useFormik } from 'formik';
 import * as yup from 'yup';
 
@@ -7,14 +7,25 @@ const validationSchema = yup.object({
   name: yup.string().required('Debe ingresar su nombre.'),
   lastName: yup.string().required('Debe ingresar su apellido.'),
   email: yup.string().email('Debe ingresar un email valido.').required('Debe ingresar un email.'),
-  password: yup.string().min(6).required('Debe ingresar una contraseña.')
+  password: yup.string().min(6, 'La contraseña debe tener al menos 6 caracteres').required('Debe ingresar una contraseña.'),
+  confirmPassword: yup.string().required('Debe confirmar su contraseña.').when("password", {
+    is: val => (val && val.length > 0 ? true : false),
+    then: yup.string().oneOf(
+      [yup.ref("password")],
+      "Las contraseñas no coinciden"
+    )
+  })
 })
 
 export default function Register() {
 
   const formik = useFormik({
     initialValues: {
-
+      name: '',
+      lastName: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
     },
     validationSchema: validationSchema,
     onSubmit: (values) => {
@@ -34,8 +45,8 @@ export default function Register() {
           label='name'
           value={formik.values.name}
           onChange={formik.handleChange}
-          error={formik.errors.name ? true : false}
-          helperText={formik.errors.name}
+          error={formik.touched.name && Boolean(formik.errors.name)}
+          helperText={formik.touched.name && formik.errors.name}
         />
         <TextField
           fullWidth
@@ -44,8 +55,8 @@ export default function Register() {
           label='lastNmae'
           value={formik.values.lastName}
           onChange={formik.handleChange}
-          error={formik.errors.lastName ? true : false}
-          helperText={formik.errors.lastName}
+          error={formik.touched.lastName && Boolean(formik.errors.lastName)}
+          helperText={formik.touched.name && formik.errors.lastName}
         />
         <TextField
           fullWidth
@@ -54,19 +65,34 @@ export default function Register() {
           label='email'
           value={formik.values.email}
           onChange={formik.handleChange}
-          error={formik.errors.email ? true : false}
-          helperText={formik.errors.email}
+          error={formik.touched.email && Boolean(formik.errors.email)}
+          helperText={formik.touched.email && formik.errors.email}
         />
         <TextField
           fullWidth
           id='password'
           name='password'
           label='password'
+          type='password'
           value={formik.values.password}
           onChange={formik.handleChange}
-          error={formik.errors.password ? true : false}
-          helperText={formik.errors.password}
+          error={formik.touched.password && Boolean(formik.errors.password)}
+          helperText={formik.touched.password && formik.errors.password}
         />
+        <TextField
+          fullWidth
+          id='confirmPassword'
+          name='confirmPassword'
+          label='confirmPassword'
+          type='password'
+          value={formik.values.confirmPassword}
+          onChange={formik.handleChange}
+          error={formik.touched.confirmPassword && Boolean(formik.errors.confirmPassword)}
+          helperText={formik.touched.confirmPassword && formik.errors.confirmPassword}
+        />
+        <Button color="primary" variant="contained" fullWidth type="submit">
+          Submit
+        </Button>
       </form>
     </div>
   )
