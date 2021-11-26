d42e6a0d5adc2eed8d538249113eae7257442d63
FranciscoMessina <francisco@messina.ws>
2021-11-26 20:46:38 -0300

    Adding Slider and depencies required.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import ImageSlider from './ImageSlider/ImageSlider'
 
 function Home() {
     return (
         <div>
             <h1>Home Component</h1>
+            <ImageSlider/>
         </div>
     )
 }
