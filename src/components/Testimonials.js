35854811340bda50c4be9cc412190e6c7927b3b0
FranciscoMessina <francisco@messina.ws>
2021-11-26 22:22:57 -0300

    Organized files and final touches

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import LoginForm from './LoginForm'
 
 function Testimonials() {
     return (
         <>
             <h1>Testimonials...</h1> 
-            <LoginForm />  
         </>
     )
 }
