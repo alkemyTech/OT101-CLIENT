1432ff12db9e4293a0b39b2e0042ec424843511a
German Sanz <64764636+germansanz93@users.noreply.github.com>
2021-11-30 01:06:10 -0300

    Ot101 23 register form (#9)
    * installed mui
    
    * created Register form component
    
    * started with form inputs
    
    * formik validations done
    
    * form styled
    
    * form done
    
    * styling
    
    Co-authored-by: hfoncea <hfoncea@yahoo.com>

---
+++ src/styles/RegisterFormStyles.js
@@ -0,0 +1,19 @@
+export default {
+  innerBox: {
+    border: '1px',
+    padding: '40px',
+    borderRadius: '4px', 
+    borderColor: 'lightgrey', 
+    borderStyle: 'solid' 
+  },
+  form: {
+    marginTop: '20px'
+  },
+  textField: {
+    marginBottom: '20px'
+  },
+  button: {
+    marginTop: '30px',
+    padding: '10px',
+  }
+}
