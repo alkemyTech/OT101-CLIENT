b4845ae2bcd6de7276592983504b5bfdae3bfeea
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2021-12-03 22:26:57 -0300

    Create Auth service

---
+++ src/services/authService.js
@@ -0,0 +1,24 @@
+import * as httpService from '../services/requestsHandlerService';
+
+export async function login(data) {
+  const { firstName, lastName, email, token } = await httpService.postRequest('TODO ADD ENDPOINT', data);
+  
+  localStorage.setItem('token', token);
+
+  return { firstName, lastName, email };
+}
+
+export async function register(data) {
+  await httpService.postRequest('TODO ADD ENDPOINT', data);
+}
+
+export async function auth() {
+  if (localStorage.getItem('token')) {
+    //Enviar token y obtener la data correspondiente al usuario, siempre y cuando el token sea válido
+
+  }
+}
+
+export function logout() {
+  localStorage.clear('token');
+}
