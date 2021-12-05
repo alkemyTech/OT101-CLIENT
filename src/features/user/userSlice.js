ec830c6d2acb780b6137096894bdb3bed7fae9de
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2021-12-05 20:57:43 -0300

    Add redux thunks

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,4 +1,9 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk, isAnyOf } from '@reduxjs/toolkit';
+import * as authService from '../../services/authService';
+
+const login = createAsyncThunk('users/login', (data) => authService.login(data));
+
+const verifyToken = createAsyncThunk('users/verifyToken', () => authService.verifyToken());
 
 export const userSlice = createSlice({
   name: 'user',
@@ -7,19 +12,29 @@ export const userSlice = createSlice({
     isTokenVerified: false,
   },
   reducer: {
-    verifyAuth: (state) => {
-      state.isTokenVerified = true;
-    },
-    login: (state, action) => {
-      state.isLogged = true;
-      state.data = action.payload;
-    },
-    logout: (state) => {
+    clearData: (state) => {
       state.isLogged = false;
       state.data = null;
     },
   },
+  extraReducers: (builder) => {
+    builder.addCase(login.fulfilled, (state, action) => {
+      state.isLogged = true;
+      state.data = action.payload;
+    });
+
+    builder.addCase(verifyToken.fulfilled, (state, action) => {
+      state.isLogged = true;
+      state.data = action.payload;
+    });
+    
+    builder.addMatcher(isAnyOf(verifyToken.fulfilled, verifyToken.rejected), (state) => {
+      state.isTokenVerified = true;
+    });
+
+  },
 });
 
-export const { login, logout, verifyAuth } = userSlice.actions;
+export const { clearData } = userSlice.actions;
+export { login, verifyToken };
 export default userSlice.reducer;
