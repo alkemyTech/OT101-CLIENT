3687a6de13f57c63281a473a1328816a0c9e7fe7
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2021-12-05 20:57:07 -0300

    Complete verifyToken function authService

--- src/services/authService.js
+++ src/services/authService.js
@@ -2,20 +2,23 @@ import * as httpService from '../services/requestsHandlerService';
 
 export async function login(data) {
   const { firstName, lastName, email, token } = await httpService.postRequest('TODO ADD ENDPOINT', data);
-  
+
   localStorage.setItem('token', token);
 
   return { firstName, lastName, email };
 }
 
-export async function register(data) {
-  await httpService.postRequest('TODO ADD ENDPOINT', data);
+export function register(data) {
+  return httpService.postRequest('TODO ADD ENDPOINT', data);
 }
 
-export async function auth() {
+export async function verifyToken() {
   if (localStorage.getItem('token')) {
     //Enviar token y obtener la data correspondiente al usuario, siempre y cuando el token sea válido
     const { firstName, lastName, email, token } = await httpService.getRequest('TODO ADD ENDPOINT');
+    localStorage.setItem('token', token);
+
+    return { firstName, lastName, email };
   }
 }
 
