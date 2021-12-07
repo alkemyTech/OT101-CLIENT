4b5c8e71a7b750b9cdfc530090baf2e000540dca
alejandropascuale <apascuale@outlook.com>
2021-12-07 17:58:52 -0300

    add state to store last news

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,20 @@
 import ImageSlider from '../components/ImageSlider/ImageSlider';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import { getRequest } from '../services/requestsHandlerService'
 
 const Home = () => {
   const user = useSelector((state) => state.user);
+  
+  const [news, setNews] = useState([]);
+  const endPointNews = '';
+
+  useEffect(() => {
+    getRequest(endPointNews).then(response => setNews(response.reverse().slice(0, 3)));
+  }, [])
+
   return (
     <div>
       <h1>{user.isLoggin === true? `Hola ${user.firstName}, bienvenido a Fundación somos más`: 'Bienvenido a Fundación somos más'}</h1>
