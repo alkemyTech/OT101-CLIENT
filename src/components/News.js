e20b3a58c02f7409990dcd69ed31bf13c89aef61
alejandropascuale <apascuale@outlook.com>
2021-12-07 17:15:59 -0300

    delete console log state from news.js

--- src/components/News.js
+++ src/components/News.js
@@ -1,7 +1,6 @@
 import Grid from '@mui/material/Grid';
 
 const News = ({ news }) => {
-  console.log(state);
   return (
     <Grid item xs={12} sm={6} lg={3} sx={{ display: 'flex', justifyContent: 'center' }}>
       <img src={news.image} alt={news.title} />
