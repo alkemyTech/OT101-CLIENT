16ccfb76c68969372ecbee35ee1bb12850821732
alejandropascuale <apascuale@outlook.com>
2021-12-07 17:02:58 -0300

    add dynamic message if the user is logged in

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,17 @@
 import ImageSlider from '../components/ImageSlider/ImageSlider';
 import React from 'react';
-import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 
-const Home = ({ title = 'MENSAJE DE BIENVENIDA' }) => {
+const Home = ({ title }) => {
+  const user = useSelector((state) => state.user);
   return (
     <div>
-      <h1>Home Component</h1>
+      <h1>{user.isLoggin === true? title=`Hola ${user.firstName}, bienvenido a Fundación somos más`: title='Bienvenido a Fundación somos más'}</h1>
       <ImageSlider />
 
       <Box sx={{ flexGrow: 1, textAlign: 'center' }}>
-        <h1>{title}</h1>
         <h2>ÚLTIMAS NOVEDADES</h2>
         <Grid container spacing={2}></Grid>
       </Box>
