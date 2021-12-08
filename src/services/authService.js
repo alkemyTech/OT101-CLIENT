ab82acfb07756e5aa9087755b8b32c548ae590fd
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2021-12-08 15:23:49 -0300

    OT101-70 logout (#17)
    * Create Auth service
    
    * Create Auth Hook
    
    * Implement useAuth hook in App component
    
    * Add verify token action in user reducer
    
    * Remove console.log
    
    * Implement http service in authService.auth function
    
    * Remove useAuth hook
    
    * Complete verifyToken function authService
    
    * Add redux thunks
    
    * Add dispatch to verify token and load user session
    
    * Clear localStorage token if verifyToken responds 401
    
    Co-authored-by: hfoncea <hfoncea@yahoo.com>

---
+++ src/services/authService.js
@@ -0,0 +1,32 @@
+import * as httpService from '../services/requestsHandlerService';
+
+export async function login(data) {
+  const { firstName, lastName, email, token } = await httpService.postRequest('TODO ADD ENDPOINT', data);
+
+  localStorage.setItem('token', token);
+
+  return { firstName, lastName, email };
+}
+
+export function register(data) {
+  return httpService.postRequest('TODO ADD ENDPOINT', data);
+}
+
+export async function verifyToken() {
+  if (localStorage.getItem('token')) {
+    //Enviar token y obtener la data correspondiente al usuario, siempre y cuando el token sea válido
+    try {
+      const { firstName, lastName, email, token } = await httpService.getRequest('TODO ADD ENDPOINT');
+      localStorage.setItem('token', token);
+  
+      return { firstName, lastName, email };
+    } catch (err) {
+      if (err.response.status === 401) 
+        localStorage.clear('token')     
+    }
+  }
+}
+
+export function logout() {
+  localStorage.clear('token');
+}
