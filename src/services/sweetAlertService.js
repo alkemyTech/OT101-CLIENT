5b33d06a8082c647f6e06f846bc611abf9065c62
alejandropascuale <apascuale@outlook.com>
2021-12-10 12:16:24 -0300

    re write code in sweet alert service

--- src/services/sweetAlertService.js
+++ src/services/sweetAlertService.js
@@ -2,45 +2,26 @@ import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
 const MySwal = withReactContent(Swal);
 
-function useSweetAlert(action, title, message) {
-    switch (action) {
-        case 'sucess':
-            MySwal.fire(
-                title,
-                message,
-                'success'
-            )
-            break;
-        case 'confirm':
-            MySwal.fire({
-                title,
-                message,
-                showCancelButton: true,
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    MySwal.fire('Saved!', '', 'success')
-                } else {
-                    MySwal.fire('Canceled!', '', 'error')
-                }
-            })
-            break;
-        case 'cancel':
-            MySwal.fire(
-                title,
-                message,
-                'error'
-            )
-            break;
-        case 'info':
-            MySwal.fire(
-                title,
-                message,
-                'info'
-            )
-            break;
-        default:
-            break;
+function sweetAlertService(title, message, icon, showCancelButton) {
+    if(showCancelButton === false) {
+        MySwal.fire(
+            title,
+            message,
+            icon
+        )
+    } else {
+        MySwal.fire({
+            title,
+            message,
+            showCancelButton: true,
+        }).then((result) => {
+            if (result.isConfirmed) {
+                MySwal.fire('Acción confirmada!', '', 'success')
+            } else {
+                MySwal.fire('Acción Cancelada!', '', 'error')
+            }
+        })
     }
 }
 
-export default useSweetAlert
+export default sweetAlertService
