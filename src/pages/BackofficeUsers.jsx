63e3da979041800a2f2d4a759b552abba1407854
Francisco Messina <francisco@messina.ws>
2021-12-11 17:59:55 -0300

    Display table completed

--- src/pages/BackofficeUsers.jsx
+++ src/pages/BackofficeUsers.jsx
@@ -6,45 +6,77 @@ import TableContainer from "@mui/material/TableContainer"
 import TableHead from "@mui/material/TableHead"
 import TableRow from "@mui/material/TableRow"
 import Paper from "@mui/material/Paper"
-import { Container } from "@mui/material"
+import { Button, Container, Link, Box } from "@mui/material"
+import { Link as RouterLink } from "react-router-dom"
 
 function createData(name, calories, fat, carbs, protein) {
 	return { name, calories, fat, carbs, protein }
 }
 
-const rows = [{ firstName: "John" }]
+const rows = [
+	{ firstName: "John", lastName: "Smith", email: "john@smith.com", id: 1 },
+	{ firstName: "John", lastName: "Smith", email: "john@smith.com", id: 2 },
+	{ firstName: "John", lastName: "Smith", email: "john@smith.com", id: 3 },
+	{ firstName: "John", lastName: "Smith", email: "john@smith.com", id: 4 },
+	{ firstName: "John", lastName: "Smith", email: "john@smith.com", id: 5 },
+	{ firstName: "John", lastName: "Smith", email: "john@smith.com", id: 6 },
+	{ firstName: "John", lastName: "Smith", email: "john@smith.com", id: 7 },
+	{ firstName: "John", lastName: "Smith", email: "john@smith.com", id: 8 },
+]
 
-export default function DenseTable() {
+export default function BackofficeUsers() {
+	const deleteUser = async id => {
+		alert("Eliminar usuario con ID: " + id)
+	}
+
 	return (
-		<Container maxWidth='lg'>
-			<TableContainer component={Paper}>
-				<Table sx={{ minWidth: 650 }} size='small' aria-label='table'>
-					<TableHead>
-						<TableRow>
-							<TableCell>Nombre</TableCell>
-							<TableCell align='right'>Apellido</TableCell>
-							<TableCell align='right'>Email</TableCell>
-							<TableCell align='right'>Editar</TableCell>
-							<TableCell align='right'>Eliminar</TableCell>
-						</TableRow>
-					</TableHead>
-					<TableBody>
-						{rows.map(row => (
-							<TableRow
-								key={row.name}
-								sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
-								<TableCell component='th' scope='row'>
-									{row.name}
-								</TableCell>
-								<TableCell align='right'>{row.calories}</TableCell>
-								<TableCell align='right'>{row.fat}</TableCell>
-								<TableCell align='right'>{row.carbs}</TableCell>
-								<TableCell align='right'>{row.protein}</TableCell>
+		<Box sx={{ marginTop: "32px", marginBottom: "32px" }}>
+			<Container maxWidth='xl'>
+				<TableContainer component={Paper}>
+					<Table sx={{ minWidth: 650 }} size='small' aria-label='table'>
+						<TableHead>
+							<TableRow>
+								<TableCell>Email</TableCell>
+								<TableCell align='right'>Nombre</TableCell>
+								<TableCell align='right'>Apellido</TableCell>
+								<TableCell align='right'>Editar</TableCell>
+								<TableCell align='right'>Eliminar</TableCell>
 							</TableRow>
-						))}
-					</TableBody>
-				</Table>
-			</TableContainer>
-		</Container>
+						</TableHead>
+						<TableBody>
+							{rows.map(row => (
+								<TableRow
+									key={row.id}
+									sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+									<TableCell component='th' scope='row'>
+										{row.email}
+									</TableCell>
+									<TableCell align='right'>{row.firstName}</TableCell>
+									<TableCell align='right'>{row.lastName}</TableCell>
+									<TableCell align='right'>
+										<Link
+											to={`/backoffice/users/${row.id}`}
+											underline='none'
+											component={RouterLink}>
+											<Button color='primary' size='small'>
+												Editar
+											</Button>
+										</Link>
+									</TableCell>
+									<TableCell align='right'>
+										<Button
+											color='error'
+											size='small'
+											onClick={() => deleteUser(row.id)}>
+											Eliminar
+										</Button>
+									</TableCell>
+								</TableRow>
+							))}
+						</TableBody>
+					</Table>
+				</TableContainer>
+			</Container>
+		</Box>
 	)
 }
