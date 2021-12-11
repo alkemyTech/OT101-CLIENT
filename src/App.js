b3c192c346028ff3e52a4c20f4e2555ced575512
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2021-12-11 03:23:01 -0300

    Remove unused styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
 
 import { useSelector } from 'react-redux';
 import RouteProtection from './components/RouteProtection';
