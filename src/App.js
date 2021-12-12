d37496ceae7b4f624c96b2d9b850e515f55b522b
Francisco Messina <francisco@messina.ws>
2021-12-12 13:11:20 -0300

    Added form to edit user info

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
+import React from "react"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import "./App.css"
 
-import About from './pages/About';
-import Activities from './pages/Activities';
-import Contact from './pages/Contact';
-import Contribute from './pages/Contribute';
-import Error404 from './pages/Error404';
-import Home from './pages/Home';
-import Loading from './components/Loading';
-import News from './pages/News';
-import Testimonials from './pages/Testimonials';
-import BackofficeContacts from './pages/BackofficeContacts';
-import { useSelector } from 'react-redux';
-import Register from './pages/Register';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import RouteProtection from './components/RouteProtection';
+import About from "./pages/About"
+import Activities from "./pages/Activities"
+import Contact from "./pages/Contact"
+import Contribute from "./pages/Contribute"
+import Error404 from "./pages/Error404"
+import Home from "./pages/Home"
+import Loading from "./components/Loading"
+import News from "./pages/News"
+import Testimonials from "./pages/Testimonials"
+import BackofficeContacts from "./pages/BackofficeContacts"
+import { useSelector } from "react-redux"
+import Register from "./pages/Register"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import RouteProtection from "./components/RouteProtection"
 
 /* Routes settings
   name: Name of the route, if not set will not show
@@ -26,48 +26,54 @@ import RouteProtection from './components/RouteProtection';
   roles: Array of roles, will be pass to the RouteProtection
 */
 const routes = [
-  { name: 'Inicio', path: '/', element: <Home /> },
-  { name: 'Sobre nosotros', path: '/about', element: <About /> },
-  { name: 'Actividades', path: '/activities', element: <Activities /> },
-  { name: 'Novedades', path: '/news', element: <News /> },
-  { name: 'Testimonios', path: '/testimonials', element: <Testimonials /> },
-  { name: 'Contacto', path: '/contact', element: <Contact /> },
-  { name: 'Contribuir', path: '/contribute', element: <Contribute /> },
-  {
-    name: 'Backoffice',
-    path: '/backoffice/contacts',
-    element: <BackofficeContacts />,
-    protect: true,
-    roles: ['admin'],
-  },
-  { path: '/register', element: <Register /> },
-  { path: '*', element: <Error404 /> },
-];
+	{ name: "Inicio", path: "/", element: <Home /> },
+	{ name: "Sobre nosotros", path: "/about", element: <About /> },
+	{ name: "Actividades", path: "/activities", element: <Activities /> },
+	{ name: "Novedades", path: "/news", element: <News /> },
+	{ name: "Testimonios", path: "/testimonials", element: <Testimonials /> },
+	{ name: "Contacto", path: "/contact", element: <Contact /> },
+	{ name: "Contribuir", path: "/contribute", element: <Contribute /> },
+	{
+		name: "Backoffice",
+		path: "/backoffice/contacts",
+		element: <BackofficeContacts />,
+		protect: true,
+		roles: ["admin"],
+	},
+	{ path: "/register", element: <Register /> },
+	{ path: "*", element: <Error404 /> },
+]
 
 function App() {
-  const { isTokenVerified } = useSelector((state) => state.user);
+	const { isTokenVerified } = useSelector(state => state.user)
 
-  return (
-    <BrowserRouter>
-      {!isTokenVerified ? (
-        <Loading />
-      ) : (
-        <>
-          <Header routes={routes} />
+	return (
+		<BrowserRouter>
+			{!isTokenVerified ? (
+				<Loading />
+			) : (
+				<>
+					<Header routes={routes} />
 
-          <Routes>
-            {routes.map(({ path, element, protect, roles }) => (
-              <Route
-                path={path}
-                element={protect ? <RouteProtection roles={roles}>{element}</RouteProtection> : element}
-              />
-            ))}
-          </Routes>
-          <Footer />
-        </>
-      )}
-    </BrowserRouter>
-  );
+					<Routes>
+						{routes.map(({ path, element, protect, roles }) => (
+							<Route
+								path={path}
+								element={
+									protect ? (
+										<RouteProtection roles={roles}>{element}</RouteProtection>
+									) : (
+										element
+									)
+								}
+							/>
+						))}
+					</Routes>
+					<Footer />
+				</>
+			)}
+		</BrowserRouter>
+	)
 }
 
-export default App;
+export default App
--- src/pages/Testimonials.js
+++ src/pages/Testimonials.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react"
 
 function Testimonials() {
-    return (
-        <>
-            <h1>Testimonials...</h1> 
-        </>
-    )
+	return (
+		<>
+			<h1>Testimonials...</h1>
+		</>
+	)
 }
 
 export default Testimonials
