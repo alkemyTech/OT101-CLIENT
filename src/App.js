bc7f219b13ee9c433841ca5a98e7e33ed49bf62c
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2021-12-12 20:31:29 -0300

    Add react-router nested routes

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { useSelector } from 'react-redux';
 import RouteProtection from './components/RouteProtection';
-import routes from './routes';
 import CircularProgress from '@mui/material/CircularProgress';
 import { Box } from '@mui/system';
+import { publicRoutes, backofficeRoutes } from './routes';
+import PublicLayout from './components/PublicLayout';
+
 function App() {
   const { isTokenVerified } = useSelector((state) => state.user);
 
@@ -13,16 +15,20 @@ function App() {
     <BrowserRouter>
       {!isTokenVerified ? (
         <Box sx={{ height: '100vh', display: 'flex' }}>
-          <CircularProgress sx={{ mx: 'auto', my: 'auto', display: 'block'}} size={64} />
+          <CircularProgress sx={{ mx: 'auto', my: 'auto', display: 'block' }} size={64} />
         </Box>
       ) : (
         <Routes>
-          {routes.map(({ path, element, protect, roles }) => (
-            <Route
-              path={path}
-              element={protect ? <RouteProtection roles={roles}>{element}</RouteProtection> : element}
-            />
-          ))}
+          <Route path="/" element={<PublicLayout routes={publicRoutes} />}>
+            {publicRoutes.map(({ path, element }) => (
+              <Route path={path} element={element} />
+            ))}
+          </Route>
+          <Route path="/backoffice">
+            {backofficeRoutes.map(({ path, element, roles }) => (
+              <Route path={path} element={<RouteProtection roles={roles}>{element}</RouteProtection>} />
+            ))}
+          </Route>
         </Routes>
       )}
     </BrowserRouter>
