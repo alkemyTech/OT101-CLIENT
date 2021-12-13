ce9f50ec65177678dcee455518ccf2a649feb8d7
alejandropascuale <apascuale@outlook.com>
2021-12-13 14:43:13 -0300

    add validations to name and logo fields

--- src/pages/EditOrganization.js
+++ src/pages/EditOrganization.js
@@ -3,33 +3,42 @@ import * as yup from 'yup';
 import { Button, Container, Grid, TextField, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import { postRequest } from '../services/requestsHandlerService';
-import Swal from 'sweetalert2';
 
+const FILE_SIZE = 20 * 1024 * 1024;
+const SUPPORTED_FORMATS = [
+  "image/jpg",
+  "image/jpeg",
+  "image/gif",
+  "image/png"
+];
 
 const validationSchema = yup.object({
   name: yup
     .string()
     .required('Debe ingresar un nombre.')
     .max(250, 'Máximo 250 caracteres'),
-  email: yup
-    .string()
-    .required('Debe ingresar un email.')
-    .max(250, 'Maximo 250 caracteres')
-    .email('Debe ingresar un email válido.'),
-  phone: yup
-    .string()
-    .max(250, 'Maximo 250 caracteres'),
-  message: yup
-    .string()
-    .required('Debe ingresar la consulta'),
+  logo: yup
+    .mixed()
+    .nullable()
+    .required("Debe ingresar un archivo")
+    .test(
+      "fileSize",
+      "El archivo no puede superar los 20MB",
+      value => value && (value.size <= FILE_SIZE)
+      )
+      .test(
+        "fileFormat",
+        "Formato del archivo inválido",
+        value => value && SUPPORTED_FORMATS.includes(value.type)
+      )
 });
 
 
 const EditOrganization = () => {
-  const urlSubmit = ''
-  const doSubmit = (values, actions) => {
-    const apiRequest = postRequest(urlSubmit, values);
-
+  const urlSubmit = '';
+  const doSubmit = (formData, actions) => {
+    const apiRequest = postRequest(urlSubmit, formData);
+    
     apiRequest.then(data => {
         /* Swal.fire('Contacto', 'Solicitud procesada correctamente', 'success'); */
         actions.resetForm();
@@ -40,7 +49,7 @@ const EditOrganization = () => {
       .finally(() => actions.setSubmitting(false));
   };
 
-  const { handleChange, handleSubmit, values, errors, isSubmitting } = useFormik({
+  const { handleChange, handleSubmit, values, errors, isSubmitting, setFieldValue } = useFormik({
     initialValues: {
       name: '',
       logo: ''
@@ -49,12 +58,12 @@ const EditOrganization = () => {
     onSubmit: doSubmit,
     validateOnChange: false,
   });
-
+  console.log(values);
   return (
     <Box sx={{ my: 'auto' }}>
       <Container maxWidth="md">
         <Box
-          sx={{ border: 1, paddingX: 5, paddingY: 5, borderRadius: 0.5, borderColor: 'lightgrey' }}
+          sx={{ border: 1, paddingX: 2, paddingY: 3, borderRadius: 0.5, borderColor: 'lightgrey' }}
           justifyContent="center"
         >
           <Typography variant="h5" component="h2" textAlign="left" fontWeight="bold" mb={6}>
@@ -79,9 +88,9 @@ const EditOrganization = () => {
                   id="logo"
                   name="logo"
                   type="file"
-                  onChange={handleChange}
-                  value={values.logo}
-                  label="logo"
+                  onChange={ (event) => {
+                    setFieldValue('logo', event.currentTarget.files[0])
+                  }}
                   fullWidth
                   error={Boolean(errors.logo)}
                   helperText={errors.logo}
