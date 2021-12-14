f71331980d6f6657a36436fb3dd4fc44060cd049
Alejandro Pascuale <83843450+alejandropascuale@users.noreply.github.com>
2021-12-14 01:02:14 -0300

    Ot101 46 screen listado novedades (#26)
    * move sweet alert services to services folder
    
    * create backoffice News page
    
    * add rout to render backofficeNews page
    
    * adapt the new component to show the required fields
    
    * create screen talbes folder to save all components to usebackoffice talbles
    
    * create the EnhancedTableHead component to modularize backoffice news
    
    * modify the order of the edit and delete buttons
    
    * Modification to reuse EnhancedTableToolbar and EnhancedTableHead components
    
    Co-authored-by: hfoncea <hfoncea@yahoo.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
+import About from './pages/About';
+import Activities from './pages/Activities';
+import Contact from './pages/Contact';
+import Contribute from './pages/Contribute';
+import Error404 from './pages/Error404';
+import Home from './pages/Home';
+import Loading from './components/Loading';
+import News from './pages/News';
+import Testimonials from './pages/Testimonials';
+import BackofficeContacts from './pages/BackofficeContacts';
+import BackofficeNews from './pages/BackofficeNews';
 import { useSelector } from 'react-redux';
 //import RouteProtection from './components/RouteProtection';
 import CircularProgress from '@mui/material/CircularProgress';
@@ -20,6 +30,19 @@ function App() {
         </Box>
       ) : (
         <Routes>
+          <Route path="/" element={<Home />} />
+
+          <Route path="/about" element={<About />} />
+          <Route path="/activities" element={<Activities />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/testimonials" element={<Testimonials />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/contribute" element={<Contribute />} />
+          <Route path="/backoffice/contacts" element={<BackofficeContacts />} />
+          <Route path="/backoffice/news" element={<BackofficeNews />} />
+          {/* returns the Erorr404 component in case the path does't exist */}
+          <Route path="*" element={<Error404 />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/" element={<PublicLayout routes={publicRoutes} />}>
             {publicRoutes.map(({ path, element }) => (
               <Route key={path} path={path} element={element} />
---
+++ src/components/ScreenTables/EnhancedTableToolbar.js
@@ -0,0 +1,66 @@
+import { alpha } from '@mui/material/styles';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import FilterListIcon from '@mui/icons-material/FilterList';
+
+export const EnhancedTableToolbar = (props) => {
+  const { numSelected } = props;
+
+  return (
+    <Toolbar
+      sx={{
+        pl: { sm: 2 },
+        pr: { xs: 1, sm: 1 },
+        ...(numSelected > 0 && {
+          bgcolor: (theme) =>
+            alpha(theme.palette.primary.main, theme.palette.action.activatedOpacity),
+        }),
+      }}
+    >
+      {numSelected > 0 ? (
+        <Typography
+          sx={{ flex: '1 1 100%' }}
+          color="inherit"
+          variant="subtitle1"
+          component="div"
+        >
+          {numSelected} selected
+        </Typography>
+      ) : (
+        <Typography
+          sx={{ flex: '1 1 100%' }}
+          variant="h6"
+          id="tableTitle"
+          component="div"
+        >
+          News
+        </Typography>
+      )}
+
+      {numSelected > 0 ? (
+        <>
+          <Tooltip title="Edit">
+            <IconButton>
+              <EditIcon />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Delete">
+            <IconButton>
+              <DeleteIcon />
+            </IconButton>
+          </Tooltip>
+        </>
+      ) : (
+        <Tooltip title="Filter list">
+          <IconButton>
+            <FilterListIcon />
+          </IconButton>
+        </Tooltip>
+      )}
+    </Toolbar>
+  );
+};
