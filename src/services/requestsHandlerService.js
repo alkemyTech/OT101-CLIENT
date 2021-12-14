43a08d694bce563dd69593acb57b00ab42d297b2
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2021-12-14 02:07:31 -0300

    Refactor http service

--- src/services/requestsHandlerService.js
+++ src/services/requestsHandlerService.js
@@ -1,13 +1,13 @@
 //import axios
-import axios from "axios"
+import axios from 'axios';
 
-//config object for axios to set the headers 
-let config = {}
+//config object for axios to set the headers
+let config = {};
 
-//if exist token in localStorage, then add it to headers 
-function addToken(){
-  if(localStorage.getItem("token")){
-    config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`
+//if exist token in localStorage, then add it to headers
+function addToken() {
+  if (localStorage.getItem('token')) {
+    config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
   }
 }
 
@@ -16,7 +16,7 @@ function addToken(){
  * @param {string} url
  * @param {object} headers
  * @returns {Promise}
- * 
+ *
  * this function is used to handle get requests, receives the url and the headers,
  * if not headers are passed, then the headers object is setted to {}
  * if token exist in localStorage, then add it to headers
@@ -24,17 +24,7 @@ function addToken(){
 export function getRequest(url, headers = {}) {
   config.headers = headers;
   addToken();
-  let promise = new Promise((resolve, reject) => {
-    axios.get(url, config)
-    .then(response => {
-      resolve(response.data)
-    })
-    .catch(error => {
-      reject(error)
-    })
-  })
-
-  return promise
+  return axios.get(url, config).then((response) => response.data);
 }
 
 //handle post requests
@@ -43,25 +33,15 @@ export function getRequest(url, headers = {}) {
  * @param {object} data
  * @param {object} headers
  * @returns {Promise}
- * 
+ *
  * this function is used to handle post requests, receives the url, data and headers,
  * if not headers are passed, then the headers object is setted to {}
  * if token exist in localStorage, then add it to headers
  */
-export function postRequest(url, data ,headers = {}){
-  let promise = new Promise((resolve, reject) => {
-    config.headers = headers;
-    addToken();
-    axios.post(url, data, config)
-    .then(response => {
-      resolve(response.data)
-    })
-    .catch(error => {
-      reject(error)
-    })
-  })
-
-  return promise
+export function postRequest(url, data, headers = {}) {
+  config.headers = headers;
+  addToken();
+  return axios.post(url, data, config).then((response) => response.data);
 }
 
 //handle patch requests
@@ -70,24 +50,24 @@ export function postRequest(url, data ,headers = {}){
  * @param {object} data
  * @param {object} headers
  * @returns {Promise}
- * 
+ *
  * this function is used to handle patch requests, receives the url, data and headers,
  * if not headers are passed, then the headers object is setted to {}
  * if token exist in localStorage, then add it to headers
  */
-export function patchRequest(url, data ,headers = {}){
-  let promise = new Promise((resolve, reject) => {
-    config.headers = headers;
-    addToken();
-    axios.patch(url, data, config)
-    .then(response => {
-      resolve(response.data)
-    })
-    .catch(error => {
-      reject(error)
-    })
-  })
-
-  return promise
+export function patchRequest(url, data, headers = {}) {
+  config.headers = headers;
+  addToken();
+  return axios.patch(url, data, config).then((response) => response.data);
 }
 
+/**
+ * @param {string} url
+ * @param {object} headers
+ * @returns {Promise}
+ */
+export function deleteRequest(url, headers = {}) {
+  config.headers = headers;
+  addToken();
+  return axios.delete(url, config).then((response) => response.data);
+}
