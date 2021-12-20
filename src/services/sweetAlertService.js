8ac009c780b612933ba517eb83e712c049f0ede1
alejandropascuale <apascuale@outlook.com>
2021-12-20 08:06:23 -0300

    edit sweet alert service to create two functions

--- src/services/sweetAlertService.js
+++ src/services/sweetAlertService.js
@@ -1,27 +1,18 @@
 import Swal from 'sweetalert2';
-import withReactContent from 'sweetalert2-react-content';
-const MySwal = withReactContent(Swal);
 
-function sweetAlertService(title, message, icon, showCancelButton) {
-    if(showCancelButton === false) {
-        MySwal.fire(
-            title,
-            message,
-            icon
-        )
-    } else {
-        MySwal.fire({
-            title,
-            message,
-            showCancelButton: true,
-        }).then((result) => {
-            if (result.isConfirmed) {
-                MySwal.fire('Acción confirmada!', '', 'success')
-            } else {
-                MySwal.fire('Acción Cancelada!', '', 'error')
-            }
-        })
-    }
+export function basicAlert(title, message, icon) {
+    Swal.fire(
+        title,
+        message,
+        icon,
+    )
 }
 
-export default sweetAlertService
+export function confirmAlert (title, message, icon){
+    return Swal.fire({
+        title,
+        message,
+        icon,
+        showCancelButton: true,
+    });
+}
