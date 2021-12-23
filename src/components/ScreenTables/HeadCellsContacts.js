87eb1823a08485874491b867d67390defe0a59d6
alejandropascuale <apascuale@outlook.com>
2021-12-23 13:58:34 -0300

    create headcells contacts file

---
+++ src/components/ScreenTables/HeadCellsContacts.js
@@ -0,0 +1,47 @@
+
+const headCellsContacts = [
+  {
+    id: 'idKey',
+    numeric: false,
+    disablePadding: true,
+    label: 'ID',
+  },
+  {
+    id: 'name',
+    numeric: true,
+    disablePadding: false,
+    label: 'Nombre',
+  },
+  {
+    id: 'lastname',
+    numeric: true,
+    disablePadding: false,
+    label: 'Apellido',
+  },
+  {
+    id: 'phone',
+    numeric: true,
+    disablePadding: false,
+    label: 'Teléfono',
+  },
+  {
+    id: 'email',
+    numeric: true,
+    disablePadding: false,
+    label: 'Email',
+  },
+  {
+    id: 'message',
+    numeric: true,
+    disablePadding: false,
+    label: 'Mensaje',
+  },
+  {
+    id: 'deleteAt',
+    numeric: true,
+    disablePadding: false,
+    label: 'Eliminado',
+  },
+];
+
+export default headCellsContacts
