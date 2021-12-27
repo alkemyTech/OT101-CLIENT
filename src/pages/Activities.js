fc926692ceedb2e124b2617f9fee3f465c2b527c
alejandropascuale <apascuale@outlook.com>
2021-12-27 08:31:16 -0300

    create activities component

--- src/pages/Activities.js
+++ src/pages/Activities.js
@@ -1,8 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea, Grid } from '@mui/material';
+import { getRequest } from '../services/requestsHandlerService';
 
 function Activities() {
+  const [activities, setActivities] = useState([]);
+  useEffect(()=> {
+    getRequest('/activities')
+      .then(result => {
+        setActivities(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
-      <h1>Activities...</h1>
+    <>
+      <Typography variant='h3' component='h2' sx={{marginBottom: 4}}>Actividades</Typography>
+      <Grid container spacing={5} rowSpacing={8}>
+        {activities.map((activity, i) => (
+          <Grid item xs={12} md={4} key={i}>
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image={activity.image}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h3" component="h3">
+                    {activity.name}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </>
   );
 }
 
